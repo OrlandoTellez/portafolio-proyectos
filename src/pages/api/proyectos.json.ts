b203254d60464d3cb4e9ff1bfc50cfcd7a2eac92
Orlando <espadachinkinno@gmail.com>
2024-10-16 19:24:28 -0600

    se agrega otro proyecto

--- src/pages/api/proyectos.json.ts
+++ src/pages/api/proyectos.json.ts
@@ -13,6 +13,17 @@ export const GET: APIRoute = async({request}) => {
             demoUrl: 'https://gym-plantilla.vercel.app/',
             githubUrl: 'https://github.com/OrlandoTellez/gym'
         },
+        {
+            id: '2',
+            title: 'Landing Page Reto',
+            descripcionCorta: 'Sitio web de un reto usando react',
+            image: '/img/proyecto-landing.png',
+            url: '/proyecto/1',
+            description: 'Este es la primera página web realizada para el reto de 100 páginas web. Se trata de un sitio de gimnasio donde el negocio puede ofrecer sus servicios, en este caso, sesiones de ejercicios',
+            technologies: ['Astro', 'Javascript', 'CSS'],
+            demoUrl: 'https://react-reto.vercel.app/',
+            githubUrl: 'https://github.com/OrlandoTellez/landingPageReto'
+        },
         
     ];
 
