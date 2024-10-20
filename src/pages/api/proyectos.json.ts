69143f92638e85e031334a55620dbb508910fbf5
Orlando <espadachinkinno@gmail.com>
2024-10-19 18:15:47 -0600

    se generan las rutas dinamicas para los proyectos en modo de desarrollo

--- src/pages/api/proyectos.json.ts
+++ src/pages/api/proyectos.json.ts
@@ -18,9 +18,20 @@ export const GET: APIRoute = async({request}) => {
             title: 'Landing Page Reto',
             descripcionCorta: 'Sitio web de un reto usando react',
             image: '/img/proyecto-landing.png',
-            url: '/proyecto/1',
+            url: '/proyecto/2',
             description: 'Este es la primera página web realizada para el reto de 100 páginas web. Se trata de un sitio de gimnasio donde el negocio puede ofrecer sus servicios, en este caso, sesiones de ejercicios',
-            technologies: ['Astro', 'Javascript', 'CSS'],
+            technologies: ['React', 'Javascript', 'CSS', 'Vite'],
+            demoUrl: 'https://react-reto.vercel.app/',
+            githubUrl: 'https://github.com/OrlandoTellez/landingPageReto'
+        },
+        {
+            id: '3',
+            title: 'Home Page',
+            descripcionCorta: 'Sitio web que funciona como pagina de inicio de un servicio',
+            image: '/img/proyecto-homePage.png',
+            url: '/proyecto/3',
+            description: 'Este es la primera página web realizada para el reto de 100 páginas web. Se trata de un sitio de gimnasio donde el negocio puede ofrecer sus servicios, en este caso, sesiones de ejercicios',
+            technologies: ['HTML', 'CSS', 'Javascript'],
             demoUrl: 'https://react-reto.vercel.app/',
             githubUrl: 'https://github.com/OrlandoTellez/landingPageReto'
         },
